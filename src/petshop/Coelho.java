b35d07ccb86c04ffc4ced5f53beb132cb78c9876
Dev.Bene <jorgemiguel7462@gmail.com>
2026-09-23 22:14:18 -0300

    feat: adiciona classe coelho ao petshop

---
+++ src/petshop/Coelho.java
@@ -0,0 +1,13 @@
+package petshop;
+
+public class Coelho extends Animal {
+    public void pular() {
+        System.out.println("pulando...");
+    }
+
+    @Override
+    public void fazerSom() {
+        System.out.println("Nhac nhac");
+   }
+
+}
--- src/petshop/PetShop.java
+++ src/petshop/PetShop.java
@@ -10,14 +10,21 @@ public class PetShop {
         gatoDaVizinha.cor = "malhado";
         gatoDaVizinha.peso = 5;
 
+        Coelho meuCoelho = new Coelho();
+        meuCoelho.cor = "preto";
+        meuCoelho.peso = 5;
+
         meuCachorro.comer();
         meuCachorro.enterrarOsso();
         gatoDaVizinha.subirEmArvore();
+        meuCoelho.pular();
 
         Animal a1 = meuCachorro;
         Animal a2 = gatoDaVizinha;
+        Animal a3 = meuCoelho;
         a1.fazerSom();
         a2.fazerSom();
+        a3.fazerSom();
     }
 
     
